f607b7c0e2d8aad0124d216141c39359c211320b
Alex Dima <alexdima@microsoft.com>
2016-03-11 15:53:42 +0100

    clean up operators & motions & their functioning

--- main.js
+++ main.js
@@ -180,36 +180,56 @@ InputHandler.prototype.getStatusText = function() {
 InputHandler.prototype._interpretNormalModeInput = function() {
 	if (!this.OPERATORS) {
 		this.OPERATORS = [];
-		var defineOperator = (char, canRepeat, takesFurtherArgs, run) => {
+		var defineOperator = (char, run) => {
 			this.OPERATORS.push({
 				char: char,
-				run: run,
-				canRepeat: canRepeat,
-				takesFurtherArgs: takesFurtherArgs
+				run: run
+				// ,
+				// canRepeat: canRepeat,
+				// takesFurtherArgs: takesFurtherArgs
+			});
+		};
+		var defineOperatorWithMotion = (char, run) => {
+			defineOperator(char, (args) => {
+				var motion = findMotion(args);
+				if (!motion) {
+					return false;
+				}
+				return run(motion);
 			});
 		};
 		
-		defineOperator('h', /*canRepeat*/ true, /*takesFurtherArgs*/false, () => {this._cursorLeft(); return true;});
-		defineOperator('j', /*canRepeat*/ true, /*takesFurtherArgs*/false, () => {this._cursorDown(); return true;});
-		defineOperator('k', /*canRepeat*/ true, /*takesFurtherArgs*/false, () => {this._cursorUp(); return true;});
-		defineOperator('l', /*canRepeat*/ true, /*takesFurtherArgs*/false, () => {this._cursorRight(); return true;});
-		defineOperator('x', /*canRepeat*/ true, /*takesFurtherArgs*/false, () => {this._deleteCharUnderCursor(); return true;});
-		defineOperator('i', /*canRepeat*/false, /*takesFurtherArgs*/false, () => {this._setMode(INSERT_MODE); return true;});
-		defineOperator('a', /*canRepeat*/false, /*takesFurtherArgs*/false, () => {this._cursorRight(true); this._setMode(INSERT_MODE); return true;});
-		defineOperator('A', /*canRepeat*/false, /*takesFurtherArgs*/false, () => {this._cursorEndOfLine(true); this._setMode(INSERT_MODE); return true;});
-		defineOperator('d', /*canRepeat*/ true, /*takesFurtherArgs*/ true, (furtherArgs) => {
-			if (furtherArgs.length === 0) {
-				// waiting for motion
-				return false;
-			}
-			var motion = findMotion(furtherArgs[0]);
-			if (!motion) {
-				// invalid input, play (beep) sound :)
-				return true;
-			}
+		defineOperator('x', () => {
+			this._deleteCharUnderCursor();
+			return true;
+		});
+		defineOperator('i', () => {
+			this._setMode(INSERT_MODE);
+			return true;
+		});
+		defineOperator('a', () => {
+			var newPos = this._motion_cursor_right(activePosition());
+			setPositionAndReveal(newPos.line, newPos.character);
+			this._setMode(INSERT_MODE);
+			return true;
+		});
+		defineOperator('A', () => {
+			var newPos = this._motion_end_of_line(activePosition());
+			setPositionAndReveal(newPos.line, newPos.character);
+			this._setMode(INSERT_MODE);
+			return true;
+		});
+		
+		defineOperatorWithMotion('d', (motion) => {
 			this._deleteTo(motion.run());
 			return true;
-		}, true, true);
+		});
+		
+		this.CHAR_TO_OPERATOR = {};
+		for (var i = 0; i < this.OPERATORS.length; i++) {
+			var operator = this.OPERATORS[i];
+			this.CHAR_TO_OPERATOR[operator.char] = operator;
+		}
 	}
 	
 	if (!this.MOTIONS) {
@@ -221,175 +241,82 @@ InputHandler.prototype._interpretNormalModeInput = function() {
 			});
 		};
 		
-		defineMotion('w', () => this._motion_w());
-		defineMotion('e', () => this._motion_e());
-		defineMotion('$', () => this._motion_$());
-	}
-	
-	var repeatCount = 1;
-	var stringInputPart = this._currentInput;
-
-	var repeatCountMatch = stringInputPart.match(/^(\d+)/);
-	if (repeatCountMatch) {
-		repeatCount = parseInt(repeatCountMatch[0], 10);
-		stringInputPart = stringInputPart.substr(repeatCountMatch[0].length);
-	}
-	
-	if (stringInputPart.length === 0) {
-		// still building input
-		return;
-	}
-	
-	var findMotion = (char) => {
+		defineMotion('w', (pos) => this._motion_w(pos));
+		defineMotion('e', (pos) => this._motion_e(pos));
+		defineMotion('$', (pos) => this._motion_end_of_line(pos));
+		defineMotion('0', (pos) => this._motion_start_of_line(pos));
+		defineMotion('h', (pos) => this._motion_cursor_left(pos));
+		defineMotion('j', (pos) => this._motion_cursor_down(pos));
+		defineMotion('k', (pos) => this._motion_cursor_up(pos));
+		defineMotion('l', (pos) => this._motion_cursor_right(pos));
+		
+		this.CHAR_TO_MOTION = {};
 		for (var i = 0; i < this.MOTIONS.length; i++) {
 			var motion = this.MOTIONS[i];
-			if (char === motion.char) {
-				// bingo!
-				return motion;
-			}
+			this.CHAR_TO_MOTION[motion.char] = motion;
 		}
-		return null;
-	};
-	
-	var motion = findMotion(stringInputPart[0]);
-	if (motion) {
-		// it is a motion!
-		for (var cnt = 0; cnt < repeatCount; cnt++) {
-			var newPos = motion.run();
-			setPositionAndReveal(newPos.line, newPos.character);
-		}
-		this._currentInput = '';
-		return;
 	}
 	
-	var findOperator = (char) => {
-		for (var i = 0; i < this.OPERATORS.length; i++) {
-			var operator = this.OPERATORS[i];
-			if (char === operator.char) {
-				return operator;
-			}
-		}
-	};
-	
-	var operator = findOperator(stringInputPart[0]);
-	if (!operator) {
-		// invalid input, play (beep) sound :)
-		this._currentInput = '';
-		return;
-	}
-	
-	if (!operator.canRepeat) {
-		repeatCount = 1;
-	}
-	var clearInput = false;
-	for (var cnt = 0; cnt < repeatCount; cnt++) {
-		clearInput = operator.run(stringInputPart.substr(1)) || clearInput;
-	}
-	if (clearInput) {
-		this._currentInput = '';
-	}
-	
-	
-	// // Check if it is an operator
-	// for (var i = 0; i < this.OPERATORS.length; i++) {
-	// 	var operator = this.OPERATORS[i];
-	// 	if (stringInputPart[0] === operator.char) {
-	// 		// it is an operator!
-	// 		if (operator.takesMotionArg) {
-	// 			if (stringInputPart.length === 1) {
-	// 				// no motion argument yet, still building input
-	// 				return;
-	// 			}
-	// 			var motion = 
-	// 		}
-	// 	}
-	// }
-	// return;
-	// console.log('_interpretInput: ', this._currentInput);
-	// console.log('repeatCount: ', repeatCount);
-	// console.log('stringInputPart: ', stringInputPart);
-	// throw new Error('TODO!');
+	var findMotion = (input) => {
+		var repeatCount = 1;
+		var stringInputPart = input;
 
-	// var clear = false;
-	// switch(this._currentInput) {
-	// 	case 'h':
-	// 		this._cursorLeft();
-	// 		clear = true;
-	// 		break;
-	// 	case 'j':
-	// 		this._cursorDown();
-	// 		clear = true;
-	// 		break;
-	// 	case 'k':
-	// 		this._cursorUp();
-	// 		clear = true;
-	// 		break;
-	// 	case 'l':
-	// 		this._cursorRight(false);
-	// 		clear = true;
-	// 		break;
-	// 	case 'x':
-	// 		this._deleteCharUnderCursor();
-	// 		clear = true;
-	// 		break;
-	// 	case 'i':
-	// 		this._setMode(INSERT_MODE);
-	// 		return;
-	// 	case 'a':
-	// 		this._cursorRight(true);
-	// 		this._setMode(INSERT_MODE);
-	// 		return;
-	// 	case 'A':
-	// 		this._cursorEndOfLine(true);
-	// 		this._setMode(INSERT_MODE);
-	// 		return;
-	// 	case 'dw':
-	// 		this._deleteTo(this._motion_w());
-	// 		clear = true;
-	// 		break;
-	// 	case 'de':
-	// 		this._deleteTo(this._motion_e());
-	// 		clear = true;
-	// 		break;
-	// 	case 'd$':
-	// 		this._deleteTo(this._motion_$());
-	// 		clear = true;
-	// 		break;
-	// 	case 'w':
-	// 		var newPos = this._motion_w();
-	// 		setPositionAndReveal(newPos.line, newPos.character);
-	// 		clear = true;
-	// 		break;
-	// 	case 'e':
-	// 		var newPos = this._motion_e();
-	// 		setPositionAndReveal(newPos.line, newPos.character);
-	// 		clear = true;
-	// 		break;
-	// 	case '$':
-	// 		var newPos = this._motion_$();
-	// 		setPositionAndReveal(newPos.line, newPos.character);
-	// 		clear = true;
-	// 		break;
-	// }
-	
-	// if (clear) {
-	// 	this._currentInput = '';
-	// } else {
+		var repeatCountMatch = stringInputPart.match(/^([1-9]\d*)/);
+		if (repeatCountMatch) {
+			repeatCount = parseInt(repeatCountMatch[0], 10);
+			stringInputPart = stringInputPart.substr(repeatCountMatch[0].length);
+		}
 		
-	// }
+		var motion = this.CHAR_TO_MOTION[stringInputPart];
+		if (!motion) {
+			return null;
+		}
+		return {
+			run: () => {
+				var newPos = activePosition();
+				for (var cnt = 0; cnt < repeatCount; cnt++) {
+					newPos = motion.run(newPos);
+				}
+				return newPos;
+			}
+		}
+	};
+	
+	var operator = this.CHAR_TO_OPERATOR[this._currentInput[0]];
+	if (operator) {
+		if (operator.run(this._currentInput.substr(1))) {
+			this._currentInput = '';
+		}
+		return;
+	}
+	
+	var motion = findMotion(this._currentInput);
+	if (motion) {
+		var newPos = motion.run();
+		setPositionAndReveal(newPos.line, newPos.character);
+		this._currentInput = '';
+		return;
+	}
+	
+	// is it motion building
+	if (/^[1-9]\d*$/.test(this._currentInput)) {
+		return;
+	}
+	
+	// beep!!
+	this._currentInput = '';
 };
-InputHandler.prototype._cursorLeft = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_cursor_left = function(pos) {
 	var doc = activeDocument();
 	var line = pos.line;
 	
 	if (pos.character > 0) {
 		this._cursorDesiredCharacter = pos.character - 1;
-		setPositionAndReveal(line, this._cursorDesiredCharacter);
+		return new vscode.Position(line, this._cursorDesiredCharacter);
 	}
+	return pos;
 };
-InputHandler.prototype._cursorDown = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_cursor_down = function(pos) {
 	var doc = activeDocument();
 	var line = pos.line;
 	
@@ -397,11 +324,11 @@ InputHandler.prototype._cursorDown = function() {
 	
 	if (line < doc.lineCount - 1) {
 		line++;
-		setPositionAndReveal(line, Math.min(this._cursorDesiredCharacter, doc.lineAt(line).text.length));
+		return new vscode.Position(line, Math.min(this._cursorDesiredCharacter, doc.lineAt(line).text.length));
 	}
+	return pos;
 };
-InputHandler.prototype._cursorUp = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_cursor_up = function(pos) {
 	var doc = activeDocument();
 	var line = pos.line;
 	
@@ -409,30 +336,20 @@ InputHandler.prototype._cursorUp = function() {
 	
 	if (line > 0) {
 		line--;
-		setPositionAndReveal(line, Math.min(this._cursorDesiredCharacter, doc.lineAt(line).text.length));
+		return new vscode.Position(line, Math.min(this._cursorDesiredCharacter, doc.lineAt(line).text.length));
 	}
+	return pos;
 };
-InputHandler.prototype._cursorEndOfLine = function(allowLastPositionOnLine) {
-	var pos = activePosition();
+InputHandler.prototype._motion_cursor_right = function(pos) {
 	var doc = activeDocument();
 	var line = pos.line;
-	var maxCharacter = (allowLastPositionOnLine ? doc.lineAt(line).text.length : doc.lineAt(line).text.length - 1);
-	
-	if (pos.character !== maxCharacter) {
-		this._cursorDesiredCharacter = maxCharacter;
-		setPositionAndReveal(line, this._cursorDesiredCharacter);
-	}
-};
-InputHandler.prototype._cursorRight = function(allowLastPositionOnLine) {
-	var pos = activePosition();
-	var doc = activeDocument();
-	var line = pos.line;
-	var maxCharacter = (allowLastPositionOnLine ? doc.lineAt(line).text.length : doc.lineAt(line).text.length - 1);
+	var maxCharacter = doc.lineAt(line).text.length;
 	
 	if (pos.character < maxCharacter) {
 		this._cursorDesiredCharacter = pos.character + 1;
-		setPositionAndReveal(line, this._cursorDesiredCharacter);
+		return new vscode.Position(line, this._cursorDesiredCharacter);
 	}
+	return pos;
 };
 InputHandler.prototype._deleteCharUnderCursor = function() {
 	var pos = activePosition();
@@ -446,13 +363,15 @@ InputHandler.prototype._deleteTo = function(toPos) {
 		builder.delete(new vscode.Range(pos.line, pos.character, toPos.line, toPos.character));
 	});
 };
-InputHandler.prototype._motion_$ = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_end_of_line = function(pos) {
 	var doc = activeDocument();
 	return new vscode.Position(pos.line, doc.lineAt(pos.line).text.length);
 };
-InputHandler.prototype._motion_w = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_start_of_line = function(pos) {
+	var doc = activeDocument();
+	return new vscode.Position(pos.line, 0);
+};
+InputHandler.prototype._motion_w = function(pos) {
 	var doc = activeDocument();
 	var lineContent = doc.lineAt(pos.line).text;
 	
@@ -465,7 +384,7 @@ InputHandler.prototype._motion_w = function() {
 	
 	if (!nextWord) {
 		// return end of the line
-		return this._motion_$();
+		return this._motion_end_of_line(pos);
 	}
 	
 	if (nextWord.start <= pos.character && pos.character < nextWord.end) {
@@ -476,15 +395,15 @@ InputHandler.prototype._motion_w = function() {
 			return new vscode.Position(pos.line, nextNextWord.start);
 		} else {
 			// return end of line
-			return this._motion_$();
+			return this._motion_end_of_line(pos);
 		}
 	} else {
 		// return start of the next word
+		console.log('_motion_w: returning', new vscode.Position(pos.line, nextWord.start));
 		return new vscode.Position(pos.line, nextWord.start);
 	}
 };
-InputHandler.prototype._motion_e = function() {
-	var pos = activePosition();
+InputHandler.prototype._motion_e = function(pos) {
 	var doc = activeDocument();
 	var lineContent = doc.lineAt(pos.line).text;
 	
@@ -497,7 +416,7 @@ InputHandler.prototype._motion_e = function() {
 	
 	if (!nextWord) {
 		// return end of the line
-		return this._motion_$();
+		return this._motion_end_of_line(pos);
 	}
 	
 	// return start of the next word
